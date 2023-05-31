094b2e903cc478d4e9c388cb6d6df73981b00c31
Daniel Juarez <00012922@uca.edu.sv>
2023-05-31 11:42:34 -0600

    Add: Segundo examen corto

---
+++ SegundoCorto/src/segundocorto/SegundoCorto.java
@@ -0,0 +1,13 @@
+package segundocorto;
+
+/**
+ * 00012922 Daniel Isaías Juárez Gutiérrez, sección 01
+ * 00117222 Paola Alexandra Pérez Pérez, sección 01
+ */
+public class SegundoCorto {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
